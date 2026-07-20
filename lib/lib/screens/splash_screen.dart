c6695e3fca40c12e8beddd1cabf61297a8500082
subhadeepmalakar987-blip <subhadeepmalakar987@gmail.com>
2026-07-20 07:22:59 +0530

    Update splash_screen.dart

--- lib/lib/screens/splash_screen.dart
+++ lib/lib/screens/splash_screen.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
 
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Text(
+          'AniDraw Studio',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
